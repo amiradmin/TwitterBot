c63b6ef48993dbf7513c043a60c1aeebbf19db42
root <Amiradmin>
2021-08-21 09:09:11 +0430

    final

--- mysite/classes/designTwitter.py
+++ mysite/classes/designTwitter.py
@@ -4,8 +4,8 @@ class Twitter():
     """This class manipulate Tittwer API """
     userID = None   #Target Twitter user id
     def __init__(self):
-        self.auth = tweepy.OAuthHandler('BEKlyQhmhfBJ4MWtgHu6lKZ90', 'PPhCfYZvYwKSzNkowSEQ2K2FEHR54wejU8WNqILlKKp79Dgbjj')
-        self.auth.set_access_token('959812651-uYaji4Ibc7hxJiGs7g8HtovLeNbb91U0P51yjlFE', '567d5wjaIzy8RerGpAjNfDoSDjiVeseLzvtlpF7j2bgZw')
+        self.auth = tweepy.OAuthHandler('YONn12EyeOHGILv3VwzuN4PWJ', '3BhrxDja35CY8nLHKSM5oNkob1EK0HfeYTIvFJr9OtUm4HVHHG')
+        self.auth.set_access_token('1426439628857516035-dBswkTY1a9Xvgo9HetiepwDb4kP9Wj', '6JcJyrxb7P0dKvfh7SkijjJjIxGsf7Scn43VQJgnMAZLy')
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True)
         self.userID = 'UrlTestBot'
 
