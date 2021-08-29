43d11307cc39d6356a36acfe67af0342c7b33344
Amir <amirbehvandi747@gmail.com>
2021-08-29 10:37:53 +0430

    final

--- mysite/twiterapi/views.py
+++ mysite/twiterapi/views.py
@@ -9,8 +9,9 @@ from classes.designTwitter import Twitter
 
 class MentionList(APIView):
     """
-    List all snippets, or create a new snippet.
+        This class get content of a tweet if someone mentions my account
     """
+
     def get(self, request, format=None):
 
         twObj = Twitter()
@@ -19,20 +20,8 @@ class MentionList(APIView):
             menObj = Mention()
             print("Here:"+mem.in_reply_to_status_id_str)
             print(mem.in_reply_to_screen_name)
-
-            # menObj.accountName = mem.in_reply_to_screen_name
-            # menObj.tweetID = mem.in_reply_to_status_id
-            # menObj.tweet = mem.text
-            # menObj.tweetDate = mem.created_at
-
-
-
-            # menObj.tweet = tweet.text
-            # menObj.tweetID = tweet.id_str
             reply = twObj.get_tweet_by_id(mem.id)
             print(reply.full_text)
-
-            user_email = "abc@gmail.com"
 
             keywords = ["save", "Save", "SAVE"]
             result = any(keyword in reply.full_text for keyword in keywords)
@@ -43,18 +32,13 @@ class MentionList(APIView):
                     menObj.tweetID = mem.in_reply_to_status_id
                     menObj.accountName = mem.in_reply_to_screen_name
                     print(mem.id)
-                    # menObj.tweetDate = tweet.created_at
                     menObj.tweet = tweet.full_text
-                    # menObj.imageUrl = tweet.user.profile_image_url
                     menObj.followers =int(tweet.user.followers_count)
                     menObj.name = tweet.user.name
                     menObj.description =  tweet.user.description
                     menObj.avatarUrl =  tweet.user.profile_image_url_https
 
                     menObj.following = twObj.get_following(tweet.user.id)
-                    print(tweet.user.id)
-                    print('========================')
-                    print()
                     # print(tweet)
                     if 'media' in tweet.entities:
                         for image in tweet.entities['media']:
@@ -63,12 +47,9 @@ class MentionList(APIView):
 
                     for item in tweet.entities['urls']:
                         menObj.url = item['expanded_url']
-                        # print(item['followers_count'])
-                        # menObj.url = item['url']
-                        # print( item['profile_image_url'])
 
                     menObj.save()
-            # print(tweet)
+
 
 
 
@@ -76,12 +57,3 @@ class MentionList(APIView):
         mention = Mention.objects.all()
         serializer = MentionSerializer(mention, many=True)
         return Response(serializer.data)
-
-    # def post(self, request, format=None):
-    #     serializer = SnippetSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
